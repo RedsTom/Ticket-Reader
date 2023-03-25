d3321e4aee296029233a1a26d4cebb0880da70ae
Clément Sol <clementsol0110@gmail.com>
2023-03-25 15:51:13 +0100

    Added a pointer cursor on the button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -164,6 +164,7 @@ const update = async () => {
       background-color: colors.$blue;
       border: none;
       outline: none;
+      cursor: pointer;
 
       padding: .5rem 1rem;
       border-radius: .5rem;
